047c165709112cf7ce98862647d389ce7a7a38af
Fabio Leal <sousaleal.fabio@gmail.com>
2013-05-02 01:05:29 -0300

    Adding footer spaceship

--- assets/js/parallax_animation.js
+++ assets/js/parallax_animation.js
@@ -7,4 +7,15 @@ jQuery(document).ready(function(){
         { xparallax: '300px',    yparallax: '300px' }
     )
   });
+
+
+  jQuery('body')
+  .each(function(){
+    jQuery('.parallax-light', this)
+    .parallax(
+        { mouseport: jQuery(this) },
+        { xparallax: '100px',    yparallax: '100px' }
+    )
+  });
+
 });
