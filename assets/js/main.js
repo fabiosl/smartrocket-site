af89cc876d3c63359c18220f247514d61466b46f
Fabio Leal <sousaleal.fabio@gmail.com>
2013-05-10 02:21:09 -0300

    Tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,29 @@ $(document).ready(function() {
   $('#simple-menu').sidr({
     side: 'right'
   });
+
+
+  $(window).resize(function() {
+    resize()
+    // $('body').prepend('<div>' + $(window).width() + '</div>');
+  });
+
+  resize();
 });
+
+function resize(){
+  if($(window).width() > 760){
+      $(".hideable").show();
+      $("#were-smartrocket-text").css("width","50%")
+      $("#were-smartrocket-text").css("text-align","right")
+    }
+    else {
+      $("#were-smartrocket-text").css("width","100%")
+      $(".hideable").hide();
+      $("#were-smartrocket-text").css("text-align","center")
+
+    }
+}
 
 $(window).touchwipe({
   wipeLeft: function() {
